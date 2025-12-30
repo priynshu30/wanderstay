04798c6219b1339b973699a1505d434116327946
priyanshu <priyanshukumarr444@gmail.com>
2025-12-31 00:34:26 +0530

    Delete src directory

--- src/api/blogApi.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const fetchBlogsApi = async () => {
-  const response = await axios.get(
-    "https://jsonplaceholder.typicode.com/posts"
-  );
-  return response.data.slice(0, 9);
-};
--- src/data/countries.js
+++
@@ -1,7 +0,0 @@
-export const countries = [
-  { code: "IN", name: "India" },
-  { code: "US", name: "United States" },
-  { code: "AE", name: "United Arab Emirates" },
-  { code: "FR", name: "France" },
-  { code: "JP", name: "Japan" },
-];
